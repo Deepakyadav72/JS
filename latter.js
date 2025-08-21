44386c027b370f96ae4bdd5698e452df1d8e8f26
Deepakyadav72 <deepakyadav723497@gmail.com>
2025-08-21 11:20:16 +0530

    Switch

---
+++ latter.js
@@ -0,0 +1,23 @@
+let latter ="A";
+switch (latter)
+{
+    case "A":
+        console.log("Vowel")
+        break;
+        case "E":
+        console.log("Vowel")
+        break;
+        case "I":
+        console.log("Vowel")
+        break;
+        case "O":
+        console.log("Vowel")
+        break;
+        case "U":
+        console.log("Vowel")
+        break;
+        default:
+            console.log(Consonent)
+
+
+}
---
+++ switchcase/Switch.js
@@ -0,0 +1,52 @@
+let num=6;
+
+switch (num){
+    case 1:
+        console.log("jan");
+        break;
+        case 2:
+        console.log("Feb");
+        break;
+        case 3:
+        console.log("march");
+        break;
+        case 4:
+        console.log("April");
+        break;
+        case 5:
+        console.log("May");
+        break;
+        case 6:
+        console.log("June");
+        break;
+        case 7:
+        console.log("july");
+        break;
+        case 8:
+        console.log("August");
+        break;
+        case 9:
+        console.log("sept");
+        break;
+        case 10:
+        console.log("Oct");
+        break;
+        case 11:
+        console.log("Nov");
+        break;case 12:
+        console.log("Dec");
+        break;
+        default:
+            console.log(null);
+
+
+}
+
+
+
+const arr=[1,2,3,4,5,6,7,8,9,10]
+console.log("arr:", arr.length);
+for( let i=0; i,arr.length;i++)
+{
+    console.log("i:",arr[i]);
+}
