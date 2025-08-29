5be7c731768da0466989712f264febac4ddc3e7c
Deepakyadav72 <deepakyadav723497@gmail.com>
2025-08-29 23:47:57 +0530

    29

--- Javascript/for loop,while loop, do while loop.js
+++ Javascript/for loop,while loop, do while loop.js
@@ -1,13 +1,29 @@
-const arr=[1,2,3,4,5,6,7,8,9,10]
-console.log("arr:", arr.length);
-for( let i=0; i,arr.length;i++)
-{
-    console.log("i:",arr[i]);
-}
-//for Loop
-const arr=[1,2,3,4,5,6,7,8,9,10]
-console.log("arr:", arr.length);
-for( let i=0; i,arr.length;i++)
-{
-    console.log("i:",arr[i]);
+// const arr=[1,2,3,4,5,6,7,8,9,10]
+// console.log("arr:", arr.length);
+// for( let i=0; i,arr.length;i++)
+// {
+//     console.log("i:",arr[i]);
+// }
+// //for Loop
+// const arr=[1,2,3,4,5,6,7,8,9,10]
+// console.log("arr:", arr.length);
+// for( let i=0; i,arr.length;i++)
+// {
+//     console.log("i:",arr[i]);
+// }
+
+
+
+
+
+
+
+
+
+//While Loop
+let x=1;
+
+while (x<=6){
+    console.log(Hello world);
+    x++;
 }
