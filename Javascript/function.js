e11ca3c7f8b3efa8539aa1543204eeeb128ba3c8
Deepakyadav72 <deepakyadav723497@gmail.com>
2025-09-01 23:45:41 +0530

    First sept code

---
+++ Javascript/function.js
@@ -0,0 +1,39 @@
+// let number_1=0;
+// let number_2=5;
+
+// function anualDay(a,b){
+//     if(a>b){
+//         return 'this is the greater then b and the value of a is $(a}';
+
+//     }
+//     else{
+//         return 'this is the greater then a and the value of b is ${b}';
+//     }
+// }
+
+// let ans =anualDay(number_1,number_2);
+// console.log()
+
+
+
+
+
+
+// Arrow function
+
+let number_1=0;
+let number_2=5;
+
+const anualDay=(a,b)=>{
+    if(a>b){
+        return 'this is the greater then b and the value of a is $(a}';
+
+    }
+    else{
+        return 'this is the greater then a and the value of b is ${b}';
+    }
+}
+
+let ans =anualDay(number_1,number_2);
+console.log()
+
---
+++ Javascript/hosting.js
@@ -0,0 +1,6 @@
+// immutable primitive data type (call by value)
+let name1='Deepak';
+let number=22
+
+let ans1=number[0]
+console.log(ans1)
