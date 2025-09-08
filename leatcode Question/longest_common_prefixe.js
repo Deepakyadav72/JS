041c4a33c87db0af3e9e3f755dba0bd02a66bd1e
Deepakyadav72 <deepakyadav723497@gmail.com>
2025-09-08 11:01:59 +0530

    function

---
+++ leatcode Question/longest_common_prefixe.js
@@ -0,0 +1,13 @@
+// let arr=["dog","racecae","car"];
+let arr =["flower","flow","flight"];
+
+function prefix(){
+    let jhola=arr[0];
+    let jhola_leng=jhola.length; 
+    for(let i=1;i<arr.length;i++){
+        while(jhola!==currentString.substring(0,jhola_leng)){
+            jhola_leng--;
+            if(jhola_leng)
+        }
+    }
+}
