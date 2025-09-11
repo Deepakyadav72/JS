c9645b9b5f37526584486e66046a8d224030e961
Deepakyadav72 <deepakyadav723497@gmail.com>
2025-09-11 14:48:45 +0530

    QA

---
+++ QuestionAnse/1if-else.js
@@ -0,0 +1,149 @@
+//Check if a number is positive, negative, or zero.
+let num = 5;
+if (num > 0) {
+  console.log("Positive number");
+} else if (num < 0) {
+  console.log("Negative number");
+} else {
+  console.log("Zero");
+}
+
+
+//Find the greater between two numbers.
+
+let num1 =10;
+let num2 = 20;
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else if (num1 < num2) {
+  console.log("num1 is less than num2");
+} else {
+  console.log("num1 is equal to num2");
+}
+
+
+//Find the largest among three numbers.
+
+let num1 =12;
+let num2 =24;
+let num3 =18;
+if (num1 > num2 && num1 > num3) {
+  console.log("num1 is the greatest");
+} else if (num2 > num1 && num2 > num3) {
+  console.log("num2 is the greatest");
+} else {
+  console.log("num3 is the greatest");
+}
+
+
+
+//Check if a given year is a leap year or not. 
+ 
+ let year =2025;
+if (year % 4 === 0 && year % 100 !== 0) {
+  console.log(year + " is a leap year.");
+} else if (year % 400 === 0) {
+  console.log(year + " is a leap year.");
+} else {
+  console.log(year + " is not a leap year.");
+}
+
+// A student’s score is given. Print the **grade** based on:
+let marks = 85;
+if (marks >= 90) {
+  console.log("Grade: A");
+} else if (marks >= 80) {
+  console.log("Grade: B");
+} else if (marks >= 70) {
+  console.log("Grade: C");
+} else {
+  console.log("Grade: D");
+}
+
+
+
+
+//Categorize a person by age
+
+let age = 20;
+
+  if (age < 13) {
+    console.log("Child");
+  } else if (age < 18) {
+    console.log("Teenager");
+  } else if (age < 60) {
+    console.log("Adult");
+  } else {
+    console.log("Senior");
+  } 
+
+
+
+  
+// Check if a given character is a vowel or consonant.
+
+let char = 'a';
+
+if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+  console.log("Vowel");
+} else {
+  console.log("Consonant");
+}
+
+
+
+//Check if a number is divisible by both 3 and 5.
+
+let num = 15;
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("Divisible by both 3 and 5");
+} else {
+  console.log("Not divisible by both 3 and 5");
+}
+
+
+
+//A temperature is given. Print:
+let temperature = 30;
+
+if (temperature > 25) {
+  console.log("Hot");
+} else if (temperature < 15) {
+  console.log("Cold");
+} else {
+  console.log("Warm");
+}
+
+// .....................................
+
+let object={
+    name: "pranay shinde",
+    age: 22,
+    married: "false",
+    hobbies: ["reading", "traveling", "swimming"],
+    exp:{
+        coName: "ABC Corp",
+        role: "Software Engineer",
+        duration: "2 years",
+        salary: "60,000"
+    },
+    myfunc: () => {
+        return "Hello, pranay here!";}
+}
+let nnn = object.myfunc();
+console.log(nnn);
+
+console.log(object.name);
+console.log(object.age);
+console.log(object.married);
+console.log(object.hobbies);
+console.log(object.exp);
+for(let key in object){
+    console.log(`${key}: ${object[key]}`);
+
+}
+//& arry INDEXING
+ let arr = [[1, 2], 3, 4, 5];
+
+
---
+++ QuestionAnse/function.js
@@ -0,0 +1,78 @@
+
+//Write a function that takes two numbers as input and returns their sum.
+let a = 5;
+let b = 10;
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(a, b));
+
+
+
+
+//Create a function that checks if a number is even or odd.
+let i=7;
+
+function isEvenOrOdd(i) {
+  if (i % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+console.log("you are " + isEvenOrOdd(i));
+
+
+
+//Create a function that converts Celsius to Fahrenheit.
+let temp_c = 30;
+function celsiusToFahrenheit(temp_c) {
+  return (temp_c * 9) / 5 + 32;
+}
+console.log("Temperature in Fahrenheit: " + celsiusToFahrenheit(temp_c));
+
+
+//Create a function that takes a string and returns its length.
+let str = "Hello, pranay dada!";
+function dada(str) {
+  return str.length;
+}
+console.log("Length of the string: " + dada(str));
+
+
+
+
+//Write a function that returns the square of a number.
+
+let num = 4;
+
+function square(num) {
+    return num * num;
+}
+console.log("The square of " + num + " is: " + square(num));
+
+
+
+// ........................
+let a1 =11;
+let b1 = 10;
+
+function maximum(a1,b1){
+    if(a>b){
+        console.log("a is greater");
+        return ;
+    }else{
+        console.log("b is greater");
+        return ;
+    }
+}
+
+
+
+// ....................
+function name1(Deepak){
+    return Deepak;
+}
+console.log(deepak);
